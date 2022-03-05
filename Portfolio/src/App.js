218628799cdec89312090581ede8b830b0f451ab
harrymurphy1534 <harryjmurphy1997@gmail.com>
2022-03-05 16:55:29 +0000

    content commit

--- Portfolio/src/App.js
+++ Portfolio/src/App.js
@@ -1,5 +1,16 @@
+import React from 'react'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <div className='home'>Harry Murphy A Junior Software Engineer</div>
+      <div className='about'>About Harry</div>
+      <div className='skills'>Skill Set</div>
+      <div className='projects'>Heres what i have been working on </div>
+      <div className='interests'>What i am up to when im not coding</div>
+      <div className='contact'>How to make contact</div>
+    </>
+  )
 }
 
 export default App
